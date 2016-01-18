214e064f9775efd1610808c806e7a8512b3f1810
Coronaxe <viktorfuchs@gmx.at>
2016-01-18 17:15:25 +0100

    Smooth Animations

--- init.js
+++ init.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
+  //Ensures Smooth Page Transitions
+  $("body").css("display", "none");
+  //Loads the NavBar
   $("#navbar").load("navbar.html");
+  //Fades the page in
+  $("body").fadeIn(500);
   console.log("Loaded navbar successfully!");
   $.getScript("utility.js")
     .done(function() {
